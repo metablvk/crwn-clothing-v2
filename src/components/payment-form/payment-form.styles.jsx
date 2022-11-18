350c854c0e4191b14a09ecc8353384ccdee9e4d8
Meta Blvk <metablvk@gmail.com>
2022-11-18 09:16:45 -0700

    Added in stripe

---
+++ src/components/payment-form/payment-form.styles.jsx
@@ -0,0 +1,21 @@
+import autoMergeLevel1 from 'redux-persist/es/stateReconciler/autoMergeLevel1';
+import styled from 'styled-components';
+import Button from './../button/button.component';
+
+export const PaymentFormContainer = styled.div`
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FormContainer = styled.form`
+  height: 100px;
+  min-width: 500px;
+`;
+
+export const PaymentButton = styled(Button)`
+  margin-left: auto;
+  margin-top: 30px;
+`;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: 'AIzaSyBkCUtahPXSEfB4qNK75x1CYAfBCWE6GWM',
+  authDomain: 'crwn-clothing-4fb04.firebaseapp.com',
+  projectId: 'crwn-clothing-4fb04',
+  storageBucket: 'crwn-clothing-4fb04.appspot.com',
+  messagingSenderId: '477933436435',
+  appId: '1:477933436435:web:e677f9b4e208edea2209e6',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -67,13 +67,14 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
+  // const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+  //   const { title, items } = docSnapshot.data();
+  //   acc[title.toLowerCase()] = items;
+  //   return acc;
+  // }, {});
 
-  return categoryMap;
+  // return categoryMap;
 };
 
 export const createUserDocumentFromAuth = async (
@@ -102,7 +103,7 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
-  return userDocRef;
+  return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
@@ -121,3 +122,16 @@ export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
   onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (userAuth) => {
+        unsubscribe();
+        resolve(userAuth);
+      },
+      reject
+    );
+  });
+};
